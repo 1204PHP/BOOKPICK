049ea706cde0f06968513fe34513df5841586966
yacci5513 <ckstjddh1@naver.com>
2023-12-11 12:57:45 +0900

    1211 sidebar layout

--- BOOKPICK/resources/views/layout/layout.blade.php
+++ BOOKPICK/resources/views/layout/layout.blade.php
@@ -6,10 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/common.css">
     <link rel="stylesheet" href="/css/user.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>@yield('title', 'bookpick')</title>
 </head>
 <body>
     @include('layout.sidebar')
     @yield('main')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
--- BOOKPICK/resources/views/layout/sidebar.blade.php
+++ BOOKPICK/resources/views/layout/sidebar.blade.php
@@ -1,27 +1,27 @@
-<div class="Container">
-    <div class="Header">
-        <span class="HeaderSpan">BOOK PICK'</span>
+
+  <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
     </div>
-    <!-- 로그인 영역 -->
-    <div class="LoginBar">
-        <a href="{{ route('user.login') }}">로그인 후 이용하기</a>
-    </div>
-    <!-- 도서검색 버튼 영역 -->
-    <div class="SearchBtn">
-        <button type="button">도서 검색</button>
-    </div>
-    <!-- 제 1 메뉴 영역 -->
-    <div class="MainContent1">
-        <a href="">오늘의 이슈</a>
-        <a href="">카테고리 별 도서</a>
-        <a href="">베스트셀러</a>
-        <a href="">북픽 추천 도서</a>
-        <a href="">찜 목록</a>
-    </div>
-    <br>
-    <div class="MainContent2">
-        <a href="">토론장</a>
-        <a href="">도서 요청</a>
-        <a href="">서비스 소개</a>
-    </div>
+  </nav>
+
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+    </ul>
 </div>
