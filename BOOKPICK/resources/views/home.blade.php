6aad6056cfaae9ed05b2fd12758dcba606277b92
yacci5513 <ckstjddh1@naver.com>
2023-12-15 18:11:14 +0900

    1215 공통css수정

--- BOOKPICK/resources/views/home.blade.php
+++ BOOKPICK/resources/views/home.blade.php
@@ -3,12 +3,5 @@
 @section('title', 'home')
 {{-- title로 Main 표기 --}}
 @section('content')
-	<div class="carousel-container">
-		<div class="carousel-wrapper" id="carouselWrapper">
-			<div class="carousel-item">1</div>
-			<div class="carousel-item">2</div>
-			<div class="carousel-item">3</div>
-			<!-- 추가적인 캐러셀 아이템들 -->
-		</div>
-	</div>
+	main
 @endsection
