f462422c9faba0e4ae3d0861070f45280fb31cdf
hocheol00 <ghcjf0702@naver.com>
2023-12-15 14:51:20 +0900

    1215 sub.js 오류 제거

--- BOOKPICK/public/js/sub.js
+++ BOOKPICK/public/js/sub.js
@@ -1,16 +1,22 @@
 // 주간 베스트 버튼, 캐러셀 영역
-document.addEventListener('DOMContentLoaded', function () {
+
     const track = document.querySelector('.best-gird-box1');
-    const items = document.querySelectorAll('.img-best1');
+    const items = document.querySelectorAll('.imgsize');
     const itemsPerGroup = 2; // 한 번에 넘어갈 아이템의 수
     const groupWidth = 100 / itemsPerGroup; // 각 그룹의 너비 (백분율)
-
+    const nextbtn = document.querySelector('.carousel-next');
+    const prevbtn = document.querySelector('.carousel-prev');
     let currentIndex = 0;
 
 
 	function updateCarousel() {
 		const translateXValue = currentIndex * groupWidth;
 		track.style.transform = `translateX(${-translateXValue}%)`;
+        const lastGroupIndex = Math.ceil(items.length / itemsPerGroup) - 1;
+        const isLastGroup = currentIndex >= lastGroupIndex * itemsPerGroup;
+
+    // isLastGroup을 통해 마지막 그룹이라면 다음 버튼을 숨깁니다.
+        nextbtn.style.display = isLastGroup ? 'none' : 'block';
 	}
 
     function nextSlide() {
@@ -20,19 +26,18 @@ document.addEventListener('DOMContentLoaded', function () {
             updateCarousel();
         }
 			const lastGroupIndex = Math.ceil(items.length / itemsPerGroup);
-			document.querySelector('.carousel-next').style.display = currentIndex > lastGroupIndex ? 'block' : 'none';
-			// 마지막 도착시 다음 버튼 숨김
+			nextbtn.style.display = currentIndex < lastGroupIndex * itemsPerGroup ? 'block' : 'none';
     }
 
     function prevSlide() {
         currentIndex = Math.max(currentIndex - itemsPerGroup, 0);
         updateCarousel();
-		document.querySelector('.carousel-next').style.display = 'block';
+		nextbtn.style.display = 'block';
     }
 
-    document.querySelector('.carousel-next').addEventListener('click', nextSlide);
-    document.querySelector('.carousel-prev').addEventListener('click', prevSlide);
-});
+    
+    nextbtn.addEventListener('click', nextSlide);
+    prevbtn.addEventListener('click', prevSlide);
 
 // 월간 베스트 버튼, 캐러셀 영역 (버튼 클레스 달라서 따로 만듬)
 document.addEventListener('DOMContentLoaded', function () {
