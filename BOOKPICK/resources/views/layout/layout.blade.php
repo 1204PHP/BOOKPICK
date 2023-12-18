883b42b57f23aa3553321154b1c13fef4bfa42f0
muhwa91 <muhwa_91@naver.com>
2023-12-18 14:58:09 +0900

    1218 로그인 레이아웃 구성 수정

--- BOOKPICK/resources/views/layout/layout.blade.php
+++ BOOKPICK/resources/views/layout/layout.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/common.css') }}" rel="stylesheet">
-    {{-- <link href="{{ asset('css/user.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/user.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <link href="{{ asset('css/sub.css') }}" rel="stylesheet"> --}}
     <script src="{{ asset('/js/common.js') }}" defer></script>
-    {{-- <script src="{{ asset('/js/user.js') }}" defer></script>
-    <script src="{{ asset('/js/home.js') }}" defer></script>
+    <script src="{{ asset('/js/UserValidation.js') }}" defer></script>
+    {{-- <script src="{{ asset('/js/home.js') }}" defer></script>
     <script src="{{ asset('/js/sub.js') }}" defer></script> --}}
     <title>@yield('title', 'bookpick')</title>
 </head>
--- BOOKPICK/resources/views/user_login.blade.php
+++ BOOKPICK/resources/views/user_login.blade.php
@@ -10,34 +10,24 @@
 	{{-- form 태그에서는 의도하지 않은 요청을 악의적으로 전송하여 다른 유저계정에서 실행되는 액션을 
 		트리거하는 공격방어 목적으로 @csrf 사용 --}}
 	<div class="login-container">
-		<p class="login-p">간편로그인</p>
+		<p class="login-p">BOOK PICK'</p>
 		<br>
 		<div class="login-kakaologin-button-area">
-			<button class="login-kakaologin-button" type="button">카카오계정으로 로그인</button>
+			
 		</div>
-		<div class="login-line-or-line">
-			<div class="login-line"></div>
-			<span class="login-or">OR</span>
-			<div class="login-line"></div>
-		</div>
-		<p class="login-p">이메일 로그인</p>
-		<br>
 		<div class="login-input-area">
 		<input class="login-input" type="text" id="u_email" name="u_email" value=""
 		required autocomplete="off" placeholder="이메일 주소">
-		<span class="u_mail_errormsg"></span>
 		</div>
 		<div class="login-input-area">
 		<input class="login-input" type="password" id="u_password" name="u_password" 
 		required autocomplete="off" placeholder="비밀번호">
-		<span class="u_password_errormsg"></span>
 		</div>
-		<br>
 		<div class="login-button-area">
 			<button class="login-button" type="submit">로그인</button>
-		</div>
-		<div class="login-text">
-			<span>북픽에 처음이신가요?</span> <a class="login-register-link" href="{{ route('getRegister') }}">회원가입하기</a>
+			<button class="login-kakaologin-button" type="button">카카오계정으로 로그인</button>
+			<span class="login-span">개인정보 보호를 위해 공용 PC에서 사용 시 SNS계정의 로그아웃 상태를 꼭 확인해 주세요.</span> 
+			<button class="login-register-button" type="button" onclick="{{ route('getRegister') }}">회원가입</button>
 		</div>
 	</div>
 </form>
