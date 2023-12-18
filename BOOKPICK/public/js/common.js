9b69de3eec86af33e7f84beab5a00df3b40e3793
yacci5513 <ckstjddh1@naver.com>
2023-12-18 13:13:15 +0900

    1218 헤더레이아웃 수정

--- BOOKPICK/public/js/common.js
+++ BOOKPICK/public/js/common.js
@@ -1,6 +1,16 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
+const signin = document.querySelector('.header_signin');
+const toggleBtn1 = document.querySelector('.navbar__toggleBtn1');
+const logo = document.querySelector('.logo_txt');
+const search = document.querySelector('.header_search');
 
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active')
+  signin.classList.toggle('active')
+})
+
+toggleBtn1.addEventListener('click', () => {
+  logo.classList.toggle('active')
+  search.classList.toggle('active')
 })
--- BOOKPICK/resources/views/layout/header.blade.php
+++ BOOKPICK/resources/views/layout/header.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar">
-
 	<div class="navbar__logo">
-		<a href="">BOOKPICK'</a>
+		<a href="" class="logo_txt">BOOK PICK'</a>
 	</div>
 
 	<ul class="navbar__menu">
@@ -9,12 +8,32 @@
 		<li><a href="#">둘러보기</a></li>
 	</ul>
 	<div class="header_search">
-		<input type="search" class="header_input" placeholder="검색"/>
+		<input type="search" class="header_input" placeholder="검색어를 입력해 주세요"/>
 		<span class=icon_search></span>
+	</div>
+	<div class="header_signin">
+		@if(Auth::check())
+			{{-- 로그인 후 표시될 내용 --}}
+			{{-- TODO: 클릭이벤트 드롭다운 추가 --}}
+			<a href="{{ route('getLogin') }}" class="header_signin_area">
+				<span class="signin_txt">로그인 후 이용하기</span>
+			</a>
+		@else
+			{{-- 로그인 전 표시될 내용 --}}
+			<a href="{{ route('getLogin') }}" class="header_signin_area">
+				로그인
+			</a>
+			<a href="{{ route('getLogin') }}" class="header_signin_area">
+				회원가입
+			</a>
+		@endif
 	</div>
 
 	<a href="#" class="navbar__toggleBtn">
-        <i class="fas fa-hamburger">햄버거</i>
-      </a>
+        <i>메뉴</i>
+    </a>
+	<a href="#" class="navbar__toggleBtn1">
+        <i>검색</i>
+    </a>
     </nav>
 </nav>
