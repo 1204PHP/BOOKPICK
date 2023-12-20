529d1874ad6fd0e4a4da61de000181cfb1e5bfb5
muhwa91 <muhwa91@gmail.com>
2023-12-21 03:04:36 +0900

    1221 공통 헤더 전체 수정

--- BOOKPICK/public/js/common.js
+++ BOOKPICK/public/js/common.js
@@ -1,33 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const toggleBtn = document.getElementsByClassName('navbar__toggleBtn');
-	const menu = document.getElementsByClassName('navbar__menu');
-	const signin = document.getElementsByClassName('header_signin');
-	const toggleBtn1 = document.getElementsByClassName('navbar__toggleBtn1');
-	const logo = document.getElementsByClassName('logo_txt');
-	const search = document.getElementsByClassName('header_search');
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const mobileNav = document.querySelector(".mobile-nav");
 
-	if (toggleBtn.length > 0) {
-		toggleBtn[0].addEventListener('click', function () {
-			for (let i = 0; i < menu.length; i++) {
-				menu[i].classList.toggle('active');
-			}
-			for (let i = 0; i < signin.length; i++) {
-				signin[i].classList.toggle('active');
-			}
-		});
-	}
+    hamburgerMenu.addEventListener("click", function () {
+        mobileNav.style.display = (mobileNav.style.display === "flex") ? "none" : "flex";
+        hamburgerMenu.classList.toggle("open");
+    });
 
-	if (toggleBtn1.length > 0) {
-		toggleBtn1[0].addEventListener('click', function () {
-			for (let i = 0; i < logo.length; i++) {
-				logo[i].classList.toggle('active');
-			}
-			for (let i = 0; i < search.length; i++) {
-				search[i].classList.toggle('active');
-			}
-		});
-	}
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 768) {
+            mobileNav.style.display = "none";
+            hamburgerMenu.classList.remove("open");
+        }
+    });
 });
+
+// document.querySelector('.hamburger-menu').addEventListener('click', function() {
+//     document.querySelector('.mobile-nav').classList.toggle('open');
+// });
+
+
+
+
+
+
+
+
+// 수정 전 코드 1
+
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const toggleBtn = document.getElementsByClassName('navbar__toggleBtn');
+// 	const menu = document.getElementsByClassName('navbar__menu');
+// 	const signin = document.getElementsByClassName('header_signin');
+// 	const toggleBtn1 = document.getElementsByClassName('navbar__toggleBtn1');
+// 	const logo = document.getElementsByClassName('logo_txt');
+// 	const search = document.getElementsByClassName('header_search');
+
+// 	if (toggleBtn.length > 0) {
+// 		toggleBtn[0].addEventListener('click', function () {
+// 			for (let i = 0; i < menu.length; i++) {
+// 				menu[i].classList.toggle('active');
+// 			}
+// 			for (let i = 0; i < signin.length; i++) {
+// 				signin[i].classList.toggle('active');
+// 			}
+// 		});
+// 	}
+
+// 	if (toggleBtn1.length > 0) {
+// 		toggleBtn1[0].addEventListener('click', function () {
+// 			for (let i = 0; i < logo.length; i++) {
+// 				logo[i].classList.toggle('active');
+// 			}
+// 			for (let i = 0; i < search.length; i++) {
+// 				search[i].classList.toggle('active');
+// 			}
+// 		});
+// 	}
+// });
 
 
 
--- BOOKPICK/resources/views/layout/header.blade.php
+++ BOOKPICK/resources/views/layout/header.blade.php
@@ -4,44 +4,43 @@ $currentRoute = Route::currentRouteName();
 @endphp
 
 @if(!in_array(Route::currentRouteName(), ['getLogin', 'getRegister', 'postLogin', 'postRegister']))
-<nav class="navbar">
-	<div class="navbar__logo">
-		<a href="{{ route('home') }}" class="logo_txt">BOOK PICK'</a>
-	</div>
-
-	<ul class="navbar__menu">
-		<li><a href="{{ route('getLibrary') }}" class="header-link {{ $currentRoute == 'library' ? 'active' : '' }}">나의 서재</a></li>
-		<li><a href="{{ route('bookTour') }}" class="header-link {{ $currentRoute == 'bookTour' ? 'active' : '' }}">둘러보기</a></li>
-	</ul>
-	<div class="header_search">
-		<form action="{{ route('getsearch.index') }}" method="get">
-			<input type="search" name="result" value="" class="header_input" placeholder="검색어를 입력해 주세요"/>
-			<button type="submit">Search</button>
-		</form>
-	</div>
-	<div class="header_signin">
+	<header>
+		<div class="hamburger-menu">
+		<div class="bar"></div>
+		<div class="bar"></div>
+		<div class="bar"></div>
+		</div>
+		<div class="navbar-logo">
+			<a href="{{ route('home') }}">BOOK PICK'</a>
+		</div>
+		<nav class="desktop-nav">
+			<a href="{{ route('getLibrary') }}" class="header-link {{ $currentRoute == 'library' ? 'active' : '' }}">나의 서재</a>
+			<a href="{{ route('bookTour') }}" class="header-link {{ $currentRoute == 'bookTour' ? 'active' : '' }}">둘러보기</a>
+			<div class="search-bar">
+				<form action="{{ route('getsearch.index') }}" method="GET">
+					<input type="search" name="result" value="" placeholder="검색어를 입력해 주세요">
+					<button type="submit">Search</button>
+				</form>
+			</div>
+		</nav>
 		@if(Auth::check())
-			{{-- 로그인 후 표시될 내용 --}}
-			<a href="{{ route('getInfo') }}" class="header_signin_area">
-				<span class="signin_txt">id</span>
-			</a>
-			<a href="{{ route('getLogout') }}" class="header_signin_area">
-				로그아웃
-			</a>
+				{{-- 로그인 후 표시될 내용 --}}
+			<nav class="header-login-button">
+				<a href="{{ route('getInfo') }}">회원정보 수정</a>
+				<a href="{{ route('getLogout') }}">로그아웃</a>
+			</nav>
 		@else
-			{{-- 로그인 전 표시될 내용 --}}
-			<a href="{{ route('getLogin') }}" class="header_signin_area">
-				로그인
-			</a>
-
+			<nav class="header-login-button">
+				<a href="{{ route('getLogin') }}">로그인</a>
+			</nav>
 		@endif
-	</div>
-
-	<button class="navbar__toggleBtn">
-        <i>메뉴</i>
-    </button>
-	<button class="navbar__toggleBtn1">
-        <i>검색</i>
-    </button>
-</nav>
+		<nav class="mobile-nav">			
+			<div class="search-bar">
+				<input type="text" placeholder="검색어를 입력해 주세요">
+				<button>Search</button>
+			</div>				
+			<a href="#">나의 서재</a></li>
+			<a href="#">둘러보기</a></li>			
+		</nav>
+	</header>
 @endif
