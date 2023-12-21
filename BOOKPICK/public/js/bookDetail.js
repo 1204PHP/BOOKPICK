c83862972dddf1a4542ff033aa1ac012597977e6
yacci5513 <ckstjddh1@naver.com>
2023-12-21 18:27:56 +0900

    1221 detail추가

--- BOOKPICK/public/js/bookDetail.js
+++ BOOKPICK/public/js/bookDetail.js
@@ -1,4 +1,4 @@
-function BookDetailshowAlert() {
+function BookDetailWishFlgshowAlert() {
 	var wishList = document.getElementById('wishFlg').value;
 	
 	console.log(wishList);
@@ -11,3 +11,33 @@ function BookDetailshowAlert() {
 		alert("찜 등록이 되었습니다.");
 	}
 }
+
+function BookDetailLibraryFlgshowAlert() {
+	var libraryFlg = document.getElementById('libraryFlg').value;
+	
+	console.log(libraryFlg);
+	if (libraryFlg === '2') {
+		alert("로그인이 필요합니다.");
+	} else if (libraryFlg === '0') {
+		alert("나의 서재에서 삭제되었습니다.");
+	} else if (libraryFlg === '1') {
+		alert("나의서재에 등록이 되었습니다.");
+	}
+}
+
+
+// document.getElementById('detailEndDate').addEventListener('click', function() {
+// 	showDatePicker();
+// });
+// function showDatePicker() {
+// 	// 현재 날짜를 가져오기
+// 	var currentDate = new Date();
+
+// 	// 사용자에게 날짜를 선택하도록 요청
+// 	var selectedDate = prompt('날짜를 선택하세요 (YYYY-MM-DD)', currentDate.toISOString().split('T')[0]);
+
+// 	// 선택한 날짜를 input 요소에 설정
+// 	if (selectedDate) {
+// 		document.getElementById('detailEndDate').value = selectedDate;
+// 	}
+// }
