d137e62fefd3282f04dc4784f225662465008439
yacci5513 <ckstjddh1@naver.com>
2023-12-22 16:55:28 +0900

    1222 서재페이지 추가

--- BOOKPICK/resources/views/library.blade.php
+++ BOOKPICK/resources/views/library.blade.php
@@ -4,20 +4,23 @@
 {{-- title로 Main 표기 --}}
 @section('content')
 	<div class="library-grid">
+		{{-- 왼쪽박스 --}}
 		<div class="library-grid-left-box">
 			<div class="library-content">
+				{{-- 제목영역 --}}
 				<div class="library-title">
 					<strong class="library-title-strong">지금까지 저장한 책이에요</strong>
 					<span class="library-media"><br></span>
-					<a href="{{ route('getLibraryFinished')}}" class="library-title-button">다 읽은 책</a>
-					<a href="{{ route('getLibraryReading')}}" class="library-title-button">읽고 있는 책</a>
-					<a href="{{ route('getLibraryWishlist')}}" class="library-title-button">찜한 책</a>
+						<a href="{{ route('getLibraryFinished') }}" class="library-title-button">다 읽은 책</a>
+						<a href="{{ route('getLibraryReading') }}" class="library-title-button">읽고 있는 책</a>
+						<a href="{{ route('getLibrarywishlist') }}" class="library-title-button">찜한 책</a>
 					<p class="library-title-p">
 						총
-						<span class="library-title_txt">10</span>
+						<span class="library-title_txt">{{$resultCnt}}</span>
 						권의 책이 있어요. 책을 눌러 감상을 기록해 보세요!
 					</p>
 				</div>
+				{{-- 책출력부분 --}}
 				<div class="search_layout_container">
 					@forelse($result as $val)
 					<div class="search_layout_container_div">
@@ -33,8 +36,12 @@
 						<p>저장된 책이 없습니다.</p>
 					@endforelse
 				</div>
+				@if($resultCnt > 0)
+					<div class="admin_pagination">{{ $result->links('pagination::default') }} </div>
+				@endif
 			</div>
 		</div>
+		{{-- 오른쪽박스 --}}
 		<div class="library-grid-right-box">
 			<div class="library-content">
 				<div class="library-title">
