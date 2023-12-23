1a72503562e84ba2feaf7d82d6ed06bdcf646a17
yacci5513 <ckstjddh1@naver.com>
2023-12-23 15:56:43 +0900

    1223 검색부분 CSS및 로그 수정

--- BOOKPICK/app/Http/Controllers/SearchController.php
+++ BOOKPICK/app/Http/Controllers/SearchController.php
@@ -11,36 +11,39 @@ class SearchController extends Controller
 {
     public function index( Request $request )
     {
-        $searchResult = trim($request->input('result'));
-        $searchStrNoSpacing = str_replace(" ","", trim($request->input('result')));
-        $searchFullTxt = str_replace(" ","* ", trim($request->input('result')))."*";
-        
-        // $request->input('result');
-        if ($searchResult) {
-            // 검색어가 제공된 경우
-            // $searchCnt = BOOK_info::WhereRaw("REPLACE(b_title,' ', '') LIKE ?", ['%' . $searchStrNoSpacing . '%'])
-            // ->orwhereRaw("REPLACE(b_author,' ', '') LIKE ?", ['%'. $searchStrNoSpacing. '%'])
-            // ->orwhereRaw("MATCH(b_title, b_author) AGAINST (? IN BOOLEAN MODE)", [$searchFullTxt])
-            // ->count();
-            $result = BOOK_info::WhereRaw("REPLACE(b_title,' ', '') LIKE ?", ['%' . $searchStrNoSpacing . '%'])
-            ->orwhereRaw("REPLACE(b_author,' ', '') LIKE ?", ['%'. $searchStrNoSpacing. '%'])
-            ->orwhereRaw("MATCH(b_title, b_author) AGAINST (? IN BOOLEAN MODE)", [$searchFullTxt])
-            ->orderByRaw("CASE WHEN REPLACE(b_title,' ', '') LIKE ? THEN 1
-                        WHEN REPLACE(b_author,' ', '') LIKE ? THEN 2 ELSE 3 END",
-                        ['%' . $searchStrNoSpacing . '%', '%' . $searchStrNoSpacing . '%'])
-            ->Paginate(18);
-            $searchCnt = $result->total();
-        } else {
-            // 검색어가 없는 경우 모든 데이터 
-            $result = Book_info::Paginate(18);
-            $searchCnt = Book_info::all()->count();
+        try {
+            Log::debug( "--------검색 시작---------" );
+            $ip = $request->ip();
+            $searchResult = trim($request->input('result'));
+            $searchStrNoSpacing = str_replace(" ","", trim($request->input('result')));
+            $searchFullTxt = str_replace(" ","* ", trim($request->input('result')))."*";
+            if ($searchResult) {
+                // 검색어가 있는 경우
+                $result = BOOK_info::WhereRaw("REPLACE(b_title,' ', '') LIKE ?", ['%' . $searchStrNoSpacing . '%'])
+                ->orwhereRaw("REPLACE(b_author,' ', '') LIKE ?", ['%'. $searchStrNoSpacing. '%'])
+                ->orwhereRaw("MATCH(b_title, b_author) AGAINST (? IN BOOLEAN MODE)", [$searchFullTxt])
+                ->orderByRaw("CASE WHEN REPLACE(b_title,' ', '') LIKE ? THEN 1
+                            WHEN REPLACE(b_author,' ', '') LIKE ? THEN 2 ELSE 3 END",
+                            ['%' . $searchStrNoSpacing . '%', '%' . $searchStrNoSpacing . '%'])
+                ->Paginate(18);
+                $searchCnt = $result->total();
+            } else {
+                // 검색어가 없는 경우 모든 데이터 
+                $result = Book_info::Paginate(18);
+                $searchCnt = $result->total();
+            }
+            Log::debug( 'ip:'.$ip.' | 검색 내용:'.$searchResult);
+            Log::debug( "--------검색 종료---------" );
+            return view('search',
+            ['result' => $result,
+            'searchResult' => $searchResult,
+            'searchCnt' => $searchCnt]);
+        } catch(Exception $e) {
+            Log::error( "--------검색 에러발생---------" );
+            Log::error( "ip:".$ip." | 검색 내용:".$searchResult);
+            Log::error( "에러내용:".$e->getMessage());
+            Log::error( "-----------------------------" );
+            return redirect()->route( 'index' );
         }
-
-        // TODO: 페이지 네이션할때 참고
-        // 'posts' => $query->paginate()->appends(request()->input())
-        return view('search',
-                    ['result' => $result,
-                    'searchResult' => $searchResult,
-                    'searchCnt' => $searchCnt]);
     }
 }
--- BOOKPICK/resources/views/search.blade.php
+++ BOOKPICK/resources/views/search.blade.php
@@ -1,14 +1,19 @@
 @extends('layout.layout')
 {{-- layout.blade.php 상속 --}}
-@section('title', 'search')
+@section('title', '검색 페이지')
 {{-- title로 Main 표기 --}}
 @section('content')
+	{{-- 검색텍스트 부분 --}}
 	<div class="search_layout_div">
 		<div class="search_layout_div_div">
-			<span>'</span><p class="search_div_div_span_txt">{{$searchResult}}</p><span>'</span>
+			<p class="search_div_div_p">'</p>
+			<p class="search_div_div_txt">{{$searchResult}}</p>
+			<p class="search_div_div_p">'</p>
 		</div>
 		<p class="search_div_div_p">에 대한 {{$searchCnt}}개의 검색 결과</p>
 	</div>
+
+	{{-- 도서 부분 --}}
 	<div class="search_layout_container">
 		@forelse($result as $val)
 		<div class="search_layout_container_div">
@@ -24,55 +29,39 @@
 			<p>검색어 결과가 없습니다.</p>
 		@endforelse
 	</div>
-	@if(count($result) > 0)
-	<div class="search_paginate_div">
-		@php
-			// $currentUrl = url()->full();
-			// $currentUrl = request()->getQueryString();
-			$currentPage = $result->currentPage();
-			$lastPage = $result->lastPage();
-			$numToShow = 5; // 한 번에 표시할 페이지 번호의 개수
-			$start = max(1, $currentPage - 2);
-			$end = min($start + 4, $lastPage);
-			$start = max(1, $end - 4);
-		@endphp
-		<a href="{{ $result->appends(request()->query())->url(1)}}">처음</a>
-		@if ($currentPage > 1)
-			<a href="{{ $result->appends(request()->query())->previousPageUrl() }}">이전</a>
-		@endif
-		@for($i = $start; $i <= $end; $i++)
-			<a href="{{$result->appends(request()->query())->url($i)}}" @if($i == $currentPage)@endif>{{$i}}</a>
-		@endfor
-		@if ($currentPage < $lastPage )
-			<a href="{{$result->appends(request()->query())->nextPageUrl()}}">다음</a>
-		@endif
-		<a href="{{$result->appends(request()->query())->url($lastPage)}}">맨끝</a>
-	</div>
+
+	{{-- 페이징 부분 --}}
+	@if($searchCnt > 0)
+		<div class="paginate_div">
+			@php
+				$currentPage = $result->currentPage();
+				$lastPage = $result->lastPage();
+				$onFirstPage = $result->onFirstPage();
+				$onLastPage = $result->onLastPage();
+				$numToShow = 5; // 한 번에 표시할 페이지 번호의 개수
+				$start = max(1, $currentPage - 2);
+				$end = min($start + 4, $lastPage);
+				$start = max(1, $end - 4);
+			@endphp
+			@if($currentPage == $onFirstPage)
+				<a class="paginate_a paginate_disable" href="{{ $result->appends(request()->query())->url(1)}}"><<</a>
+				<a class="paginate_prenext paginate_disable" href="{{ $result->appends(request()->query())->url(1)}}">이전</a>
+			@else
+				<a class="paginate_a" href="{{ $result->appends(request()->query())->url(1)}}"><<</a>
+				<a class="paginate_prenext" href="{{ $result->appends(request()->query())->previousPageUrl() }}">이전</a>
+			@endif
+			@for($i = $start; $i <= $end; $i++)
+				<a class="@if($i == $currentPage) paginate_current @else paginate_a @endif" href="{{$result->appends(request()->query())->url($i)}}" @if($i == $currentPage)@endif>{{$i}}</a>
+			@endfor
+			@if($currentPage == $onLastPage)
+				<a class="paginate_prenext paginate_disable" href="{{$result->appends(request()->query())->url($lastPage)}}">다음</a>
+				<a class="paginate_a paginate_disable" href="{{$result->appends(request()->query())->url($lastPage)}}">>></a>
+			@else
+				<a class="paginate_prenext" href="{{$result->appends(request()->query())->nextPageUrl()}}">다음</a>
+				<a class="paginate_a" href="{{$result->appends(request()->query())->url($lastPage)}}">>></a>
+			@endif
+		</div>
 	@endif
-	{{-- <div>
-		@php
-			// $currentUrl = url()->full();
-			// $currentUrl = request()->getQueryString();
-			$currentPage = $result->currentPage();
-			$lastPage = $result->lastPage();
-			$numToShow = 5; // 한 번에 표시할 페이지 번호의 개수
-			$start = max(1, $currentPage - 2);
-			$end = min($start + 4, $lastPage);
-			$start = max(1, $end - 4);
-		@endphp
-		{{$ddddd}}
-		<a href="{{ $result->url(1)}}">처음</a>{{$currentUrl}}
-		@if ($currentPage > 1)
-			<a href="{{ $result->previousPageUrl() }}">이전</a>
-		@endif
-		@for($i = $start; $i <= $end; $i++)
-			<a href="{{$result->url($i)}}" @if($i == $currentPage)@endif>{{$i}}</a>
-		@endfor
-		@if ($currentPage < $lastPage )
-			<a href="{{$result->nextPageUrl()}}">다음</a>
-		@endif
-		<a href="{{$result->url($lastPage)}}">맨끝</a>
-	</div> --}}
 @endsection
 
 @section('defer-js')
