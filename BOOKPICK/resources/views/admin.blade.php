accbe2f7a1fc49814ee231753fa0f19a9f3c3dd5
yacci5513 <ckstjddh1@naver.com>
2024-01-04 15:38:56 +0900

    0104 home컨트롤러 admin 수정

--- BOOKPICK/resources/views/admin.blade.php
+++ BOOKPICK/resources/views/admin.blade.php
@@ -4,39 +4,6 @@
 {{-- title로 Main 표기 --}}
 @section('content')
 
-{{-- BookInfo --}}
-	<br>
-	<h1>BookInfo Table</h1>
-	<div class="table-container">
-        <table>
-            <thead>
-                <tr>
-					@foreach($bookTableColumn as $column)
-                    <th>{{$column}}</th>
-					@endforeach
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($bookTableData as $data)
-					<tr>
-						@foreach($bookTableColumn as $column)
-						<td>{{$data->$column}}</td>
-						@endforeach
-					</tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-	<div class="admin_pagination">{{ $bookTableData->links('pagination::default') }} </div>
-	<br>
-	<form action="{{route('postAdminbookInfo')}}" method="POST">
-		@csrf
-		<button type="submit" name="action" value="ItemNewAll">신간 전체 리스트 초기 책정보500개 등록</button><br>
-    	<button type="submit" name="action" value="ItemNewSpecial">주목할 만한 신간 리스트 초기 책정보500개 등록</button><br>
-    	<button type="submit" name="action" value="Bestseller">베스트셀러 초기 책정보500개 등록</button><br>
-    	<button type="submit" name="action" value="BlogBest">블로거 베스트셀러 초기 책정보500개 등록</button><br>
-
-	</form>
 {{-- bookApi --}}
 	<h1>BookApi Table</h1>
 	<div class="table-container">
@@ -68,6 +35,39 @@
     	<button type="submit" name="action" value="BlogBest">블로거 베스트셀러 api추가</button><br>
 	</form>
 	<br><br>
+{{-- BookInfo --}}
+	<br>
+	<h1>BookInfo Table</h1>
+	<div class="table-container">
+		<table>
+			<thead>
+				<tr>
+					@foreach($bookTableColumn as $column)
+					<th>{{$column}}</th>
+					@endforeach
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($bookTableData as $data)
+					<tr>
+						@foreach($bookTableColumn as $column)
+						<td>{{$data->$column}}</td>
+						@endforeach
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+	<div class="admin_pagination">{{ $bookTableData->links('pagination::default') }} </div>
+	<br>
+	<form action="{{route('postAdminbookInfo')}}" method="POST">
+		@csrf
+		<button type="submit" name="action" value="ItemNewAll">신간 전체 리스트 초기 책정보500개 등록</button><br>
+		<button type="submit" name="action" value="ItemNewSpecial">주목할 만한 신간 리스트 초기 책정보500개 등록</button><br>
+		<button type="submit" name="action" value="Bestseller">베스트셀러 초기 책정보500개 등록</button><br>
+		<button type="submit" name="action" value="BlogBest">블로거 베스트셀러 초기 책정보500개 등록</button><br>
+
+	</form>
 
 {{-- ApiCate --}}
 	<h1>ApiCate Table</h1>
