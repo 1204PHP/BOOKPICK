ac580ed0d7a272167254a634811b54482974b687
muhwa91 <muhwa_91@naver.com>
2024-01-05 17:42:27 +0900

    0105 로딩애니메이션 완료

--- BOOKPICK/public/js/Loading.js
+++ BOOKPICK/public/js/Loading.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     var loader = document.querySelector('.loading-animation');
+
+    // 페이지 로딩 완료 후 숨김 처리
     loader.style.display = 'none';
+
+    // 페이지 이동 이벤트 감지
+    window.addEventListener('beforeunload', function () {
+        loader.style.display = 'flex';
+        document.body.classList.add('loading');
+    });
 });
