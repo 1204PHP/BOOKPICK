5ff200ac918a4c08debf9ccb517886e3e27f9bbd
yacci5513 <ckstjddh1@naver.com>
2024-01-13 01:15:30 +0900

    0113 댓글 대댓글 ajax 구현완료

--- BOOKPICK/public/js/bookDetail.js
+++ BOOKPICK/public/js/bookDetail.js
@@ -116,26 +116,28 @@ function limitCharacters() {
 }
 
 // 대댓글 삽입 placeholder + 글자 체크
-
-function replyHandleInput(textarea) {
+function replyHandleInput(textarea,bdc_id) {
     // 텍스트 영역의 값이 비어 있으면 레이블을 표시
-    const replylabel = document.querySelector('label[for="replycontent"]');
+	var labelName = 'label[for="replycontent'+bdc_id+'"]';
+    const replylabel = document.querySelector(labelName);
     replylabel.style.display = (textarea.value.trim() === '') ? 'block' : 'none';
 }
 
-function replyLimitCharacters() {
-    const replyInput = document.getElementById('replycontent');
-    const replyCount = document.getElementById('replycount');
-    const replyMaxCharacters = 300;
+function replyLimitCharacters(bdc_id) {
+	let replyContent='replycontent' + bdc_id;
+	let replyCount='replycount' + bdc_id;
+    const replyInputDOM = document.getElementById(replyContent);
+    const replyCountDOM = document.getElementById(replyCount);
+    const replyMaxCharacters = 700;
 
-    var replyCurrentCharacters = replyInput.value.length;
+    var replyCurrentCharacters = replyInputDOM.value.length;
 
     if (replyCurrentCharacters > replyMaxCharacters) {
-		alert("300자까지 입력할 수 있습니다.");
-		replyCurrentCharacters == 301 ? replyCurrentCharacters = 300 : replyCurrentCharacters;
-    	replyInput.value = replyInput.value.slice(0, maxCharacters);
+		alert("700자까지 입력할 수 있습니다.");
+		replyCurrentCharacters == 701 ? replyCurrentCharacters = 700 : replyCurrentCharacters;
+    	replyInputDOM.value = replyInputDOM.value.slice(0, maxCharacters);
     }
-	replyCount.textContent = `${replyCurrentCharacters}` + " / " + replyMaxCharacters;
+	replyCountDOM.textContent = `${replyCurrentCharacters}` + " / " + replyMaxCharacters;
 }
 
 function loginCheckConfirm() {
@@ -145,6 +147,9 @@ function loginCheckConfirm() {
 	}
 }
 
+
+
+// 댓글 insert
 function insertFormCheck() {
 	let contentValue = document.getElementById("content").value;
 	if (contentValue.trim() === "") {
@@ -176,7 +181,6 @@ function insertFormCheck() {
 					var newCount = currentCount + 1;
 
 					bdcHeadTxtCountDOM.innerHTML = newCount;
-					console.log(newCount);
 					
 					// 새로운 div 요소 생성
 					var newDivElement = document.createElement('div');
@@ -221,7 +225,7 @@ function insertFormCheck() {
 					bottomArea.className = 'bdc-list-bottom-area';
 			
 					var replyLink = document.createElement('a');
-					replyLink.className = 'bdc-list-area-reply';
+					replyLink.className = 'bdc-list-area-reply'
 					replyLink.textContent = '답글';
 					replyLink.onclick = function() {
 						var StrNum = commentResult['bdc_id'];
@@ -231,6 +235,7 @@ function insertFormCheck() {
 					var replyCount = document.createElement('span');
 					replyCount.className = 'bdc-list-area-reply-cnt';
 					replyCount.textContent = '0';
+					replyCount.id = 'bdc-list-area-reply-cnt' + StrNum;
 			
 					replyLink.appendChild(replyCount);
 			
@@ -296,7 +301,7 @@ function insertFormCheck() {
 }
 
 
-
+// 처음 로드시 댓글 출력
 document.addEventListener('DOMContentLoaded', function() {
     let formData = new FormData();
 	let bId = document.getElementById("bdc_b_id").value;
@@ -369,6 +374,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			var replyCount = document.createElement('span');
 			replyCount.className = 'bdc-list-area-reply-cnt';
 			replyCount.textContent = commentResult[i]['reply_count'];
+			replyCount.id = 'bdc-list-area-reply-cnt' + StrNum;
 	
 			replyLink.appendChild(replyCount);
 	
@@ -429,7 +435,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		})
 });
 
-
+// 답글 클릭시
 let clickedIds = [];
 
 function replyOpen(bdc_id) {
@@ -448,10 +454,9 @@ function replyOpen(bdc_id) {
 			let strNum = "bdc-list-area" + bdc_id;
 			let parentElement = document.getElementById(strNum);
 			let acFlg = document.getElementById("ac_flg").value;
-			let uName = document.getElementById("u_name").value;
-			console.log(data);
-			console.log(strNum);
-			console.log(parentElement);
+			if(acFlg == 1 ) {
+				var uName = document.getElementById("u_name").value;
+			}
 
 			for (let i = 0; i < replyResult.length; i++) {
 				// 새로운 댓글 영역을 생성
@@ -470,7 +475,7 @@ function replyOpen(bdc_id) {
 				// 이름 생성
 				var nameElement = document.createElement('span');
 				nameElement.className = 'bdc-list-area-name';
-				nameElement.textContent = '정**';
+				nameElement.textContent = replyResult[i]['u_name'];
 
 				// 시간 생성
 				var timeElement = document.createElement('span');
@@ -504,14 +509,13 @@ function replyOpen(bdc_id) {
 				likeImage.src = '/img/book_detail_like.png';
 
 				var likeCount = document.createElement('span');
-				likeCount.textContent = '254';
+				likeCount.textContent = replyResult[i]['like'];
 
 				likeLink.appendChild(likeImage);
 				likeLink.appendChild(likeCount);
 
 				// 싫어요 링크 생성
 				var dislikeLink = document.createElement('a');
-				dislikeLink.href = '#';
 				dislikeLink.className = 'bdc-list-area-dislike-box';
 
 				var dislikeImage = document.createElement('img');
@@ -520,7 +524,7 @@ function replyOpen(bdc_id) {
 				dislikeImage.alt = '';
 
 				var dislikeCount = document.createElement('span');
-				dislikeCount.textContent = '1';
+				dislikeCount.textContent = replyResult[i]['dislike'];
 
 				dislikeLink.appendChild(dislikeImage);
 				dislikeLink.appendChild(dislikeCount);
@@ -544,12 +548,9 @@ function replyOpen(bdc_id) {
 				parentElement.appendChild(newReplyArea);
 			}
 
-
-			console.log(data);
-			console.log(strNum);
-			console.log(parentElement);
 			var bdcReplyWriteDiv = document.createElement('div');
 			bdcReplyWriteDiv.classList.add('bdc-reply-write');
+			bdcReplyWriteDiv.id = 'bdc-reply-write'+bdc_id;
 
 			var bdcWriteDiv = document.createElement('div');
 			bdcWriteDiv.classList.add('bdc-write');
@@ -584,22 +585,23 @@ function replyOpen(bdc_id) {
 
 			var replyContentTextarea = document.createElement('textarea');
 			replyContentTextarea.classList.add('bdc-reply-write-area');
-			replyContentTextarea.id = 'replycontent';
+			replyContentTextarea.id = 'replycontent'+bdc_id;
 			replyContentTextarea.rows = '5';
 			replyContentTextarea.cols = '30';
 			replyContentTextarea.name = 'replycontent';
 			replyContentTextarea.maxlength = '301';
 
-			if( acFlg == 1 ) {
+			if( acFlg == 2 ) {
 				replyContentTextarea.onclick = loginCheckConfirm;
 			}
+			
 			replyContentTextarea.oninput = function() {
-				replyLimitCharacters();
-				replyHandleInput(this);
+				replyLimitCharacters(bdc_id);
+				replyHandleInput(this, bdc_id);
 			};
 			var writeLabel = document.createElement('label');
 			writeLabel.classList.add('bdc-write-label');
-			writeLabel.htmlFor = 'replycontent';
+			writeLabel.htmlFor = 'replycontent'+bdc_id;
 			if( acFlg == 1 ) {
 				writeLabel.innerHTML = '다양한 의견이 서로 존중될 수 있도록 다른 사람에게 불쾌감을 주는 욕설, 혐오, 비하의 ' +
 									'표현이나 타인의 권리를 침해하는 내용은 주의해주세요. ' +
@@ -621,8 +623,8 @@ function replyOpen(bdc_id) {
 
 			var replyCountSpan = document.createElement('span');
 			replyCountSpan.classList.add('bdc-write-upload-cnt');
-			replyCountSpan.id = 'replycount';
-			replyCountSpan.textContent = '0 / 300';
+			replyCountSpan.id = 'replycount'+bdc_id;
+			replyCountSpan.textContent = '0 / 700';
 
 			var uploadButton = document.createElement('button');
 			uploadButton.type = 'button';
@@ -632,7 +634,7 @@ function replyOpen(bdc_id) {
 				uploadButton.onclick = loginCheckConfirm;
 			} else if(acFlg == 1) {
 				uploadButton.onclick = function() {
-					aa();
+					replyInsertFormCheck(bdc_id);
 				};
 			}
 
@@ -647,7 +649,18 @@ function replyOpen(bdc_id) {
 
 			bdcReplyWriteDiv.appendChild(bdcWriteDiv);
 			parentElement.appendChild(bdcReplyWriteDiv);
-		
+
+			var divElement = document.createElement("div");
+			divElement.classList.add('bdc-reply-fold-box');
+			var buttonElement = document.createElement("button");
+			buttonElement.classList.add('bdc-reply-fold-item');
+			buttonElement.onclick = function() {
+				CloseReply(bdc_id);
+			};
+			buttonElement.innerHTML = "답글 접기 ∧";
+
+			divElement.appendChild(buttonElement);
+			parentElement.appendChild(divElement);
 		})
 		.catch(error => {
 			console.error('오류 발생:', error);
@@ -662,7 +675,149 @@ function replyOpen(bdc_id) {
 	}
 }
 
+// 답글 닫기
+function CloseReply(bdc_id) {
+	let strNum = "bdc-list-area" + bdc_id;
+	let parentElement = document.getElementById(strNum);
+	var childElements = parentElement.children;
+		for (var i = 0; i < childElements.length; i++) {
+			childElements[i].classList.toggle('display-none');
+		}
+}
 
-function aa() {
-	alert("성공")
+// 대댓글 insert
+function replyInsertFormCheck(bdc_id) {
+	let contentStr = 'replycontent' + bdc_id;
+	let contentValue = document.getElementById(contentStr).value;
+	if (contentValue.trim() === "") {
+		alert("내용을 입력해주세요.");
+	} else {
+		let formData = new FormData();
+			let bId = document.getElementById("bdc_b_id").value;
+			formData.append('b_id', bId);
+			formData.append('content', contentValue);
+			formData.append('bdc_id', bdc_id);
+			fetch('/book/detail/reply/insert', {
+				method: 'POST',
+				body: formData,
+			})
+			.then(response => response.json())
+			.then(data => {
+				let errorMsg = data.errorMsg;
+				if(errorMsg) {
+					if(confirm("로그인을 하신 후 이용해 주시기 바랍니다.")){
+						window.location.href = "/login";
+					}
+				} else {
+					// 주 처리부분
+					let replyResult = data.commentResult;
+					let uNameResult = data.uNameResult;
+					let contentStr = 'replycontent' + bdc_id;
+					let labelStr = 'label[for="replycontent'+bdc_id+'"]';
+					let countStr = 'replycount' + bdc_id;
+					let ParentStr = 'bdc-reply-write' + bdc_id;
+
+					let bdcListAreaReplyStr = 'bdc-list-area-reply-cnt' + bdc_id;
+					var bdcListAreaReplyDOM = document.getElementById(bdcListAreaReplyStr);
+					var currentCount = parseInt(bdcListAreaReplyDOM.innerHTML, 10);
+					var newCount = currentCount + 1;
+					bdcListAreaReplyDOM.innerHTML = newCount;
+
+					var parentElement = document.getElementById(ParentStr);
+
+					var newReplyArea = document.createElement('div');
+					newReplyArea.className = 'bdc-list-reply-area';
+	
+					// 리스트 상단 영역 생성
+					var topArea = document.createElement('div');
+					topArea.className = 'bdc-list-top-area';
+	
+					// 이미지 생성
+					var imgElement = document.createElement('img');
+					imgElement.className = 'bdc-list-area-img';
+					imgElement.src = '/img/user.png';
+	
+					// 이름 생성
+					var nameElement = document.createElement('span');
+					nameElement.className = 'bdc-list-area-name';
+					nameElement.textContent = uNameResult.u_name;
+	
+					// 시간 생성
+					var timeElement = document.createElement('span');
+					timeElement.className = 'bdc-list-area-at';
+					timeElement.textContent = new Date(replyResult['created_at'])
+												.toLocaleString('ko-KR', 
+													{year: 'numeric', month: '2-digit', day: '2-digit', hour: 'numeric', minute: 'numeric', hour12: false});
+	
+	
+					// 리스트 중단 영역 생성
+					var middleArea = document.createElement('div');
+					middleArea.className = 'bdc-list-middle-area font-1';
+	
+					var contentElement = document.createElement('p');
+					contentElement.className = 'bdc-list-area-content';
+					contentElement.textContent = replyResult['bdr_comment'];
+	
+					// 리스트 하단 영역 생성
+					var bottomArea = document.createElement('div');
+					bottomArea.className = 'bdc-list-reply-bottom-area';
+	
+					var recommendArea = document.createElement('div');
+					recommendArea.className = 'bdc-list-recommend-area';
+	
+					// 좋아요 링크 생성
+					var likeLink = document.createElement('a');
+					likeLink.className = 'bdc-list-area-like-box';
+	
+					var likeImage = document.createElement('img');
+					likeImage.className = 'bdc-dis-like-btn';
+					likeImage.src = '/img/book_detail_like.png';
+	
+					var likeCount = document.createElement('span');
+					likeCount.textContent = '0';
+	
+					likeLink.appendChild(likeImage);
+					likeLink.appendChild(likeCount);
+	
+					// 싫어요 링크 생성
+					var dislikeLink = document.createElement('a');
+					dislikeLink.className = 'bdc-list-area-dislike-box';
+	
+					var dislikeImage = document.createElement('img');
+					dislikeImage.className = 'bdc-dis-like-btn';
+					dislikeImage.src = '/img/book_detail_dislike.png';
+					dislikeImage.alt = '';
+	
+					var dislikeCount = document.createElement('span');
+					dislikeCount.textContent = '0';
+	
+					dislikeLink.appendChild(dislikeImage);
+					dislikeLink.appendChild(dislikeCount);
+	
+					recommendArea.appendChild(likeLink);
+					recommendArea.appendChild(dislikeLink);
+	
+					bottomArea.appendChild(recommendArea);
+	
+					middleArea.appendChild(contentElement);
+	
+					topArea.appendChild(imgElement);
+					topArea.appendChild(nameElement);
+					topArea.appendChild(timeElement);
+	
+					newReplyArea.appendChild(topArea);
+					newReplyArea.appendChild(middleArea);
+					newReplyArea.appendChild(bottomArea);
+
+
+					parentElement.parentNode.insertBefore(newReplyArea, parentElement);
+					document.getElementById(contentStr).value = "";
+					document.getElementById(countStr).innerHTML = "0 / 700";
+    				document.querySelector(labelStr).style.display = 'block';
+				}
+			})
+			.catch(error => {
+				console.error('오류 발생:', error);
+			})
+	}
 }
